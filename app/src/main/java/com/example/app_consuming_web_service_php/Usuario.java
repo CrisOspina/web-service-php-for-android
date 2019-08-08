64933674b223dfdf71fc2b0e22192cb31b9d3eab
Cristian Ospina <crisospina523@gmail.com>
2019-08-08 01:24:05 -0500

    clase para el control de los usuarios

---
+++ app/src/main/java/com/example/app_consuming_web_service_php/Usuario.java
@@ -0,0 +1,37 @@
+package com.example.app_consuming_web_service_php;
+
+public class Usuario {
+    private String usr, clave, nombre, correo;
+
+    public String getUsr() {
+        return usr;
+    }
+
+    public void setUsr(String usr) {
+        this.usr = usr;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+}
